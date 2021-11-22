a34719e4f8a84efaae79d5ba47c93979385e9032
ReX342 <occ.rex@gmail.com>
2021-11-22 21:39:39 +0100

    syntax of charfield

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -9,9 +9,14 @@ class NewTaskForm(forms.Form):
     # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
 
 class CreateEntry(forms.Form):
-    title = forms.CharField(label="Title"),
-    content = forms.CharField(label="New Entry"),
-    edit = forms.BooleanField(initial=False)
+    #title = forms.CharField(label="Title"),
+     title = forms.Charfiedl (
+        label="Title",
+        max_length="111"
+    
+    #content = forms.Textarea(label="New Entry"),
+    content = forms.CharField(widget=forms.Textarea, label="Entry Content"),
+    edit = forms.BooleanField(initial=True)
 
 def index(request):
     if "tasks" not in request.session:
@@ -44,9 +49,13 @@ def create(request):
         if form.is_valid():
             title = form.cleaned_data["edit"]
             #request.session["entries"] += [title]
-            # content = form.cleaned_data["content"]
+            content = form.cleaned_data["content"]
             #request.session["contents"] += [content]
-            #if util.get_entry(title) is not None:                
+            if util.get_entry(title) is not None: 
+                pass
+            # https://docs.djangoproject.com/en/3.2/ref/urlresolvers/
+            #          
+            #    util.save_entry(title,content)
             return HttpResponseRedirect(reverse("encyclopedia:index"))
         else:
             return render(request, "encyclopedia/create.html", {
